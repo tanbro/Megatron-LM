a61c08655c3f497b30885863a580d91dfd16b14b
liu xue yan <liu_xue_yan@foxmail.com>
2019-10-10 16:07:49 +0800

    modify: gentext command for 117m

--- commands/gentext_gpt2-117m-spm_zh32k.sh
+++ commands/gentext_gpt2-117m-spm_zh32k.sh
@@ -1,14 +1,13 @@
 python3 generate_samples.py \
     --model-parallel-size 1 \
     --num-layers 12 \
-    --hidden-size 1024 \
-    --num-attention-heads 16 \
+    --hidden-size 768 \
+    --num-attention-heads 12 \
     --max-position-embeddings 1024 \
-    --seq-length 128 \
-    --load "checkpoints/gpt2_180m_百科" \
+    --out-seq-length 256 \
+    --load checkpoints/gpt2_117m_hm8g \
     --tokenizer-type SentencePieceTokenizer \
     --tokenizer-path data/spm/gpt2_huamei_corpus_bpe_32k_v2.model \
     --fp16 \
     --cache-dir cache \
-    --out-seq-length 1024 \
     --top_p 1 \
