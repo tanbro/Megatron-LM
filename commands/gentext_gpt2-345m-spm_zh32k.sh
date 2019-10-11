92de8b1ab9db97aa991b33e2f491291cf5cea118
liu xue yan <liu_xue_yan@foxmail.com>
2019-10-11 17:28:52 +0800

    add: a 345M generate-text command

---
+++ commands/gentext_gpt2-345m-spm_zh32k.sh
@@ -0,0 +1,13 @@
+python3 generate_samples.py \
+    --model-parallel-size 1 \
+    --num-layers 24 \
+    --hidden-size 1024 \
+    --num-attention-heads 16 \
+    --max-position-embeddings 1024 \
+    --out-seq-length 256 \
+    --load checkpoints/gpt2_117m_hm8g \
+    --tokenizer-type SentencePieceTokenizer \
+    --tokenizer-path data/spm/gpt2_huamei_corpus_bpe_32k_v2.model \
+    --fp16 \
+    --cache-dir cache \
+    --top_p 1 \
