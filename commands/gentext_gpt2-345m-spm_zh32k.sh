f9922c72ecd06a388d0044d22af4b9286b1b8ac9
liu xue yan <liu_xue_yan@foxmail.com>
2019-10-12 10:45:08 +0800

    edit: don't know if working with PyTorch 1.3

--- commands/gentext_gpt2-345m-spm_zh32k.sh
+++ commands/gentext_gpt2-345m-spm_zh32k.sh
@@ -5,7 +5,7 @@ python3 generate_samples.py \
     --num-attention-heads 16 \
     --max-position-embeddings 1024 \
     --out-seq-length 256 \
-    --load checkpoints/gpt2_345m_hm8g \
+    --load checkpoints/gpt2_345m_hm10g \
     --tokenizer-type SentencePieceTokenizer \
     --tokenizer-path data/spm/gpt2_huamei_corpus_bpe_32k_v2.model \
     --fp16 \
