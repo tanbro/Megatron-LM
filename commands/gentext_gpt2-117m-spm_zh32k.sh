75579a209db82b5fcd534c54a729cbd068b55df2
liu xue yan <liu_xue_yan@foxmail.com>
2019-10-14 18:32:54 +0800

    edit: scripts

--- commands/gentext_gpt2-117m-spm_zh32k.sh
+++ commands/gentext_gpt2-117m-spm_zh32k.sh
@@ -7,7 +7,7 @@ python3 generate_samples.py \
     --num-attention-heads 12 \
     --max-position-embeddings 1024 \
     --out-seq-length 256 \
-    --load checkpoints/gpt2_117m_hm8g \
+    --load checkpoints/gpt2_117m_$DSNAME \
     --tokenizer-type SentencePieceTokenizer \
     --tokenizer-path data/spm/gpt2_huamei_corpus_bpe_32k_v2.model \
     --fp16 \
